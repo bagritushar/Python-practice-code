5a883c29cba14bf34bd89cf318443c3fad090f08
Tushar bagri <aryatushar179@gmail.com>
2026-03-05 11:20:45 +0530

    Added python practice program- addition of numbers

---
+++ 01_add_Two_no.py
@@ -0,0 +1,19 @@
+a = 30
+b = 70
+print("Sum of the given number is : " , a+b)
+
+
+
+
+a = int(input("Enter a first Number: "))
+b = int(input("Enter a Second Number: "))
+sum = a + b
+print("Sum of GIven Number is : ", sum)
+
+
+Num1 = float(input("Enter a first Number: "))
+Num2 = float(input("Enter a Second  Number: "))
+
+sum = Num1 + Num2
+
+print("Sum of the given number is: ", sum)
