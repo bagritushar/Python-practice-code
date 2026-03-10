850abab14e40b69a9bee3a07d7939dcaa7d0e53b
Tushar bagri <aryatushar179@gmail.com>
2026-03-10 18:40:12 +0530

    added

---
+++ ex.py
@@ -0,0 +1,8 @@
+movies = []
+mov1 = input("enter ist  movie name: ")
+mov2 = input("enter 2nd movie name: ")
+mov3 = input("enter 3rd movie name: ")
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+print(movies)
